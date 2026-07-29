2ce708a451e4f6669b520ad29853fd4b62674a9c
Natalia <nmillana@eess.cl>
2026-07-29 11:27:54 -0400

    Mostrar resumen neto pendiente

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'gm-shell-v30';
+const CACHE_NAME = 'gm-shell-v31';
 const APP_SHELL = [
   './',
   './index.html',
   './styles.css?v=20260729-1',
   './app.js?v=20260728-1',
   './fitness-challenge.js?v=20260728-1',
-  './treasury-redesign.js?v=20260729-4',
+  './treasury-redesign.js?v=20260729-5',
   './manifest.webmanifest',
   './Logo.webp',
   './icon-192.png',
